4b937cf2c99b7f6d244a00a30e5f3d116fd92487
vicorico17 <vic.cazacu@gmail.com>
2024-10-30 16:47:31 +0200

    refactor: Transform FundMe into ZombieRevive contract
    - Renamed contract to ZombieRevive
    - Added revive purchase and usage functionality
    - Implemented player tracking system
    - Added events for revive purchases and usage
    - Set revive price in USD with price feed conversion
    - Added getter functions for player revives
    - Modified fallback functions to support revive purchases

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -4,91 +4,81 @@ pragma solidity ^0.8.18;
 import {AggregatorV3Interface} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol"; 
 import {PriceConverter} from "./PriceConverter.sol";
 
-error FundMe__NotOwner();
+error ZombieRevive__NotOwner();
 
-contract FundMe {
+contract ZombieRevive {
     using PriceConverter for uint256;
 
-    mapping(address => uint256) private s_addressToAmountFunded;
-    address[] private s_funders;
+    mapping(address => uint256) private s_playerToRevivesOwned;
+    address[] private s_players;
 
-    // Could we make this constant?  /* hint: no! We should make it immutable! */
-    address private /* immutable */ i_owner;
-    uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
+    address private immutable i_owner;
+    uint256 public constant REVIVE_PRICE_USD = 1 * 10 ** 18;
 
     AggregatorV3Interface private s_priceFeed;
+
+    event RevivePurchased(address indexed player, uint256 amount);
+    event ReviveUsed(address indexed player);
 
     constructor(address priceFeedAddress) {
         i_owner = msg.sender;
         s_priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
-    function fund() public payable {
+
+    function buyRevives() public payable {
         uint256 convertedAmount = msg.value.getConversionRate(s_priceFeed);
-        require(convertedAmount >= MINIMUM_USD, "You need to spend more ETH!");
-        s_addressToAmountFunded[msg.sender] += msg.value;
-        s_funders.push(msg.sender);
+        require(convertedAmount >= REVIVE_PRICE_USD, "Not enough ETH for a revive!");
+        
+        uint256 reviveAmount = convertedAmount / REVIVE_PRICE_USD;
+        s_playerToRevivesOwned[msg.sender] += reviveAmount;
+        
+        if (s_playerToRevivesOwned[msg.sender] == reviveAmount) {
+            s_players.push(msg.sender);
+        }
+        
+        emit RevivePurchased(msg.sender, reviveAmount);
     }
 
-    function getVersion() public view returns (uint256) {
-        return s_priceFeed.version();
+    function useRevive() external {
+        require(s_playerToRevivesOwned[msg.sender] > 0, "No revives available!");
+        s_playerToRevivesOwned[msg.sender] -= 1;
+        emit ReviveUsed(msg.sender);
+    }
+
+    function getRevivesOwned() external view returns (uint256) {
+        return s_playerToRevivesOwned[msg.sender];
     }
 
     modifier onlyOwner() {
-        // require(msg.sender == owner);
-        if (msg.sender != i_owner) revert FundMe__NotOwner();
+        if (msg.sender != i_owner) revert ZombieRevive__NotOwner();
         _;
     }
 
     function withdraw() public onlyOwner {
-        for (uint256 funderIndex = 0; funderIndex < s_funders.length; funderIndex++) {
-            address funder = s_funders[funderIndex];
-            s_addressToAmountFunded[funder] = 0;
-        }
-        s_funders = new address[](0);
-        // // transfer
-        // payable(msg.sender).transfer(address(this).balance);
-
-        // // send
-        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        // require(sendSuccess, "Send failed");
-
-        // call
         (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed");
     }
-    // Explainer from: https://solidity-by-example.org/fallback/
-    // Ether is sent to contract
-    //      is msg.data empty?
-    //          /   \
-    //         yes  no
-    //         /     \
-    //    receive()?  fallback()
-    //     /   \
-    //   yes   no
-    //  /        \
-    //receive()  fallback()
 
     fallback() external payable {
-        fund();
+        buyRevives();
     }
 
     receive() external payable {
-        fund();
+        buyRevives();
     }
-    function getAddressToAmountFunded(address fundingAddress) external view returns (uint256) {
-    return s_addressToAmountFunded[fundingAddress];
-}
 
-    function getFunder(uint256 index) external view returns (address) {
-        return s_funders[index];
+    function getPlayerRevives(address player) external view returns (uint256) {
+        return s_playerToRevivesOwned[player];
     }
+
+    function getPlayer(uint256 index) external view returns (address) {
+        return s_players[index];
+    }
+
     function getOwner() external view returns (address) {
         return i_owner;
-    }   
-
-
+    }
 }
-    
 
 //view/pure
 
